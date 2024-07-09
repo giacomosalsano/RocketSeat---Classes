816ffc6fef14fd85a1ddb834280557936b5a1a7b
Giacomo Salsano <giacomosalsano@hotmail.com>
2024-07-09 16:31:05 -0300

    added the bg-pattern

--- plann_er/frontend/src/App.tsx
+++ plann_er/frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { MapPin, Calendar, ArrowRight} from 'lucide-react'
 
 export function App() {
   return (
-    <div className="h-screen flex items-center justify-center">
+    <div className="h-screen flex items-center justify-center bg-pattern bg-no-repeat bg-center">
       <div className="max-w-3xl w-full px-6 text-center space-y-10">
         <p className="text-zinc-300 text-lg">Convide seus amigos e planeje sua próxima viagem!</p>
         <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
