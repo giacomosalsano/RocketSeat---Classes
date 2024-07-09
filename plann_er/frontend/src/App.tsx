e25e99f479fc8f58ef0dc4405ce96ab7f625dfff
Giacomo Salsano <giacomosalsano@hotmail.com>
2024-07-09 15:46:20 -0300

    started the app index

--- plann_er/frontend/src/App.tsx
+++ plann_er/frontend/src/App.tsx
@@ -1,6 +1,11 @@
 export function App() {
   return (
-    <h1 className="text-3xl font-bold underline">Hello World</h1>
+    <div>
+      <p>Convide seus amigos e planeje sua próxima viagem!</p>
+      <div></div>
+      <p>Ao planejar sua viagem pela plann.er você automaticamente concorda
+      com nossos <a href="#">termos de uso</a> e <a href="#">políticas de privacidade</a>.</p>
+    </div>
   )
 }
 
