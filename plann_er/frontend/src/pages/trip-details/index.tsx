65816344c8fc3834849fc60f122b7b209b96b8f4
Giacomo Salsano <giacomosalsano@hotmail.com>
2024-07-10 14:48:00 -0300

    added CreateActivityModal file

--- plann_er/frontend/src/pages/trip-details/index.tsx
+++ plann_er/frontend/src/pages/trip-details/index.tsx
@@ -1,5 +1,6 @@
 import { Calendar, CircleCheck, CircleDashed, CircleUser, ExternalLink, Mail, MapPin, Plus, Settings2, UserCog } from "lucide-react";
 import { useState } from "react";
+import { CreateAvtivityModal } from "./CreateAvtivityModal";
 
 export function TripDetailsPage() {
   const [isCreateActivityModalOpen, setisCreateActivityModalOpen] = useState(false)
@@ -39,7 +40,7 @@ export function TripDetailsPage() {
         <div className="flex-1 space-y-6">
           <div className="flex items-center justify-between">
             <h2 className="text-3xl font-semibold">Atividades</h2>
-            <button className='bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400 '>
+            <button onClick={openCreateActivityModal} className='bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400 '>
               <Plus className='size-5' />
               Adicionar atividade
             </button>
@@ -152,6 +153,10 @@ export function TripDetailsPage() {
 
         </div>
       </main>
+
+      {isCreateActivityModalOpen && (
+        <CreateAvtivityModal closeCreateActivityModal={closeCreateActivityModal} />
+      )}
     </div>
 
     
