5259268035090f5fb7aa9a869aaa5a17016cd55e
Giacomo Salsano <giacomosalsano@hotmail.com>
2024-07-10 03:16:07 -0300

    created confirm_trip_modal

--- plann_er/frontend/src/pages/create-trip/invite-guests-modal.tsx
+++ plann_er/frontend/src/pages/create-trip/invite-guests-modal.tsx
@@ -8,7 +8,12 @@ interface InviteGuestsModalProps {
   removeEmailFromInvites: (email: string) => void
 }
 
-export function InviteGuestsModal({ addNewEmailToInvite, closeGuestsModal, emailsToInvite, removeEmailFromInvites }: InviteGuestsModalProps) {
+export function InviteGuestsModal({ 
+  addNewEmailToInvite,
+  closeGuestsModal,
+  emailsToInvite,
+  removeEmailFromInvites }: InviteGuestsModalProps) {
+    
   return (
     <div className='fixed inset-0 bg-black/60 flex items-center justify-center'>
       <div className='w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5'>
