be3c2b1563f7eff7c09a16bfea1a890c049656be
Giacomo Salsano <giacomosalsano@hotmail.com>
2024-07-10 13:02:12 -0300

    created the guests list on the tripdetailspage

--- plann_er/frontend/src/pages/trip-details/index.tsx
+++ plann_er/frontend/src/pages/trip-details/index.tsx
@@ -1,4 +1,4 @@
-import { Calendar, CircleCheck, CircleDashed, MapPin, Plus, Settings2 } from "lucide-react";
+import { Calendar, CircleCheck, CircleDashed, ExternalLink, MapPin, Plus, Settings2, UserCog } from "lucide-react";
 
 export function TripDetailsPage() {
   return (
@@ -68,7 +68,66 @@ export function TripDetailsPage() {
           </div>
         </div>
 
-        <div className="w-80 "></div>
+        <div className="w-80 space-y-6">
+          <div className="space-y-6">
+            <h2 className="font-semibold text-xl">Links Importantes</h2>
+
+            <div className="space-y-5">
+              <div className="flex items-center justify-between gap-4 bg-zinc-900 rounded-lg px-5 py-2">
+                <div className="space-y-1.5">
+                  <span className="block font-medium text-zinc-100">Reserva do AirBnB</span>
+                  <a href="#" target="_blank" className="block text-xs text-zinc-400 truncate hover:text-zinc-200">https://www.airbnb.com.br/rooms/387205887325297</a>
+                </div>
+                <a href="#" target="_blank"><ExternalLink className="size-5 text-zinc-400 shrink-0 hover:text-zinc-200"/></a>
+              </div>
+
+              <div className="flex items-center justify-between gap-4 bg-zinc-900 rounded-lg px-5 py-2">
+                <div className="space-y-1.5">
+                  <span className="block font-medium text-zinc-100">Reserva do Restaurante Tal</span>
+                  <a href="#" target="_blank" className="block text-xs text-zinc-400 truncate hover:text-zinc-200">https://www.restaurantetal.com.br/rooms/387205887325297</a>
+                </div>
+                <a href="#" target="_blank"><ExternalLink className="size-5 text-zinc-400 shrink-0 hover:text-zinc-200"/></a>
+              </div>
+
+            </div>
+
+            <button className='bg-zinc-800 w-full justify-center text-zinc-200 rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-zinc-700'>
+              <Plus className='size-5'/>
+              Cadastrar novo link
+            </button>
+          </div>
+
+          <div className='w-full h-px bg-zinc-800'/>
+
+          <div className="space-y-6">
+            <h2 className="font-semibold text-xl">Convidados</h2>
+
+            <div className="space-y-5">
+              <div className="flex items-center justify-between gap-4 ">
+                <div className="space-y-1.5">
+                  <span className="block font-medium text-zinc-100">Fulano de Tal</span>
+                  <a href="#" target="_blank" className="block text-xs text-zinc-400 truncate hover:text-zinc-200">fulanodetal@email.com.br</a>
+                </div>
+                <CircleCheck className="size-5  text-lime-300  shrink-0"/>
+              </div>
+
+              <div className="flex items-center justify-between gap-4 ">
+                <div className="space-y-1.5">
+                  <span className="block font-medium text-zinc-100">Ciclano Beltrano</span>
+                  <a href="#" target="_blank" className="block text-xs text-zinc-400 truncate hover:text-zinc-200">ciclanobeltrano@email.com</a>
+                </div>
+                <CircleDashed className="size-5 text-zinc-400 shrink-0"/>
+              </div>
+
+            </div>
+
+            <button className='bg-zinc-800 w-full justify-center text-zinc-200 rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-zinc-700'>
+              <UserCog className='size-5'/>
+              Gerenciar Convidados
+            </button>
+          </div>
+
+        </div>
       </main>
     </div>
 
