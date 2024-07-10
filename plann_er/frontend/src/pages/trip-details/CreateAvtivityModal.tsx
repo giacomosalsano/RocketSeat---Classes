c0c56726465de8473dbfb37a78a51b8f5dd1512a
Giacomo Salsano <giacomosalsano@hotmail.com>
2024-07-10 15:33:33 -0300

    finished modal

--- plann_er/frontend/src/pages/trip-details/CreateAvtivityModal.tsx
+++ plann_er/frontend/src/pages/trip-details/CreateAvtivityModal.tsx
@@ -1,43 +1,51 @@
-import { User, X } from "lucide-react";
+import { CalendarClock, Tag, X } from "lucide-react";
 
 
 interface CreateActivityModalProps {
   closeCreateActivityModal: () => void;
+  currentTime: () => string;
+  currentDate: () => string;
 }
+
 export function CreateAvtivityModal({closeCreateActivityModal}: CreateActivityModalProps) {
   return (
     <div className='fixed inset-0 bg-black/60 flex items-center justify-center'>
       <div className='w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5'>
         <div className='space-y-2'>
           <div className='flex items-center justify-between'>
-            <h2 className='text-lg font-semibold'>Confirmar criação de viagem</h2>
+            <h2 className='text-lg font-semibold'>Adicionar atividade</h2>
 
             <button onClick={closeCreateActivityModal}>
               <X className='size-5' />
             </button>
           </div>
           <p className='text-sm text-zinc-400'>
-            Para concluir a criação da viagem para <span className='text-zinc-100 font-semibold'>Florianópolis, Brasil</span> nas datas de <span className='text-zinc-100 font-semibold'>16 a 27 de Agosto de 2024</span> preencha seus dados abaixo:
+            Todos os convidados podem visualizar as atividades.
           </p>
         </div>
 
         <form className='space-y-3'>
           <div className='h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2'>
-            <User className='size-5 text-zinc-400'/>
+            <Tag className='size-5 text-zinc-400'/>
             <input 
               type="text" 
-              name='name' 
-              placeholder='Seu nome completo' 
+              name='title' 
+              placeholder='Qual a atividade?' 
               className='bg-transparent text-lg placeholder-zinc-400 outline-none flex-1' />  
           </div>
 
-          <div className='h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2'>
-            <User className='size-5 text-zinc-400'/>
-            <input 
-              type="email" 
-              name='email' 
-              placeholder='Seu e-mail pessoal' 
-              className='bg-transparent text-lg placeholder-zinc-400 outline-none flex-1' />  
+          <div className="flex items-center gap-2">
+            <div className='h-14 flex-1 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2'>
+              <CalendarClock className='size-5 text-zinc-400'/>
+              <input 
+                type="datetime-local" 
+                name='occurs_at' 
+                placeholder='Data e Horário da Atividade' 
+                className='bg-transparent text-lg placeholder-zinc-400 outline-none flex-1' />  
+            </div>
+
+    
+
           </div>
         
 
