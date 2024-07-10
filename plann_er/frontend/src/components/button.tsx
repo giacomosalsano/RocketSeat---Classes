e8ddb76ddd5bdb4c27de6551ed818aff08def82e
Giacomo Salsano <giacomosalsano@hotmail.com>
2024-07-10 16:27:04 -0300

    created button component/installed tailwindVariants

---
+++ plann_er/frontend/src/components/button.tsx
@@ -0,0 +1,29 @@
+import { ComponentProps, ReactNode } from "react"
+import { tv } from 'tailwind-variants'
+
+const buttonVariants = tv({
+  base: 'rounded-lg px-5 py-2 font-medium flex items-center gap-2',
+
+  variants: {
+    variant: {
+      primary: 'bg-lime-300 text-lime-950 hover:bg-lime-400',
+      secondary: 'bg-zinc-800 text-zinc-200 hover:bg-zinc-700',
+    }
+  },
+
+  defaultVariants: {
+    variant: 'primary',
+  }
+})
+
+interface ButtonProps extends ComponentProps<'button'> {
+  children: ReactNode
+}
+
+export function Button({children, ...props}: ButtonProps) {
+  return (
+    <button {...props} className={buttonVariants()}>
+      {children}
+    </button>
+  )
+}
